d88dfde795a5cc841a6e7bb0e023dc9bc28d3fdd
jhegstrom <32497470+jhegstrom@users.noreply.github.com>
2019-11-26 15:14:29 -0600

    Update user-audit.sh

--- user-audit/user-audit.sh
+++ user-audit/user-audit.sh
@@ -1,5 +1,4 @@
 for org in `sfdx force:org:list --json | jq -r '.result.nonScratchOrgs[] | select(.connectedStatus | contains("Connected")) '| jq -r ".alias"`
-do
-    # echo Listing users and profiles in org $org    
+do     
     sfdx force:data:soql:query --json -u $org -q "select Name, Profile.Name from User where IsActive = true order by Profile.Name" | jq --arg orgname $org -r '.result.records[] | $orgname + ", " + (.Profile.Name) + ", " + (.Name) '
 done
