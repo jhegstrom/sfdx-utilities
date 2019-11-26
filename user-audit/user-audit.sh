f3244d0df348667244e47bd3cb81e6f474966ec3
jhegstrom <32497470+jhegstrom@users.noreply.github.com>
2019-11-26 14:55:02 -0600

    Removed client filer

--- user-audit/user-audit.sh
+++ user-audit/user-audit.sh
@@ -1,4 +1,4 @@
-for org in `sfdx force:org:list --json | jq -r '.result.nonScratchOrgs[] | select(.connectedStatus | contains("Connected")) '| jq -r 'select(.username | contains("citi.com")) ' | jq -r ".alias"`
+for org in `sfdx force:org:list --json | jq -r '.result.nonScratchOrgs[] | select(.connectedStatus | contains("Connected")) '| jq -r ".alias"`
 do
     # echo Listing users and profiles in org $org    
     sfdx force:data:soql:query --json -u $org -q "select Name, Profile.Name from User where IsActive = true order by Profile.Name" | jq --arg orgname $org -r '.result.records[] | $orgname + ", " + (.Profile.Name) + ", " + (.Name) '
